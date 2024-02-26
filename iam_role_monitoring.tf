4ab9d69b0d613f7b4db351d14c85adf8f967d358
Felipe Lima Silva <felipe.lima.silva1993@gmail.com>
2024-02-26 11:38:04 -0300

    add policy document for role

--- iam_role_monitoring.tf
+++ iam_role_monitoring.tf
@@ -1,3 +1,36 @@
+#Policy
+data "aws_iam_policy_document" "rds_monitoring_policy" {
+
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:PutRetentionPolicy"
+    ]
+
+    effect = "Allow"
+    resources = [
+      "arn:aws:logs:*:*:log-group:RDS*"
+    ]
+
+    sid = "EnableCreationAndManagementOfRDSCloudwatchLogGroups"
+  }
+  statement {
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogStreams",
+      "logs:GetLogEvents"
+    ]
+
+    effect = "Allow"
+    resources = [
+      "arn:aws:logs:*:*:log-group:RDS*:log-stream:*"
+    ]
+
+    sid = "EnableCreationAndManagementOfRDSCloudwatchLogStreams"
+  }
+}
+
 resource "random_password" "rds_role_name_sufixo" {
   length      = 10
   special     = false
