a39340fa731571361bd4d2415ea119a5bab8c89f
Felipe Lima Silva <felipe.lima.silva1993@gmail.com>
2024-02-26 11:34:11 -0300

    add resource pass

--- secrets_manager.tf
+++ secrets_manager.tf
@@ -1,3 +1,9 @@
+resource "random_password" "rds_instance_password" {
+  length           = 20
+  special          = true
+  override_special = "!$%&*()[]{}<>"
+}
+
 resource "random_password" "rds_instance_name_sufixo" {
   length      = 10
   special     = false
